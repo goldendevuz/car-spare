6f993564d99231b62784b7fe0b637cdc39c0ddae
Abdulmajid Yunus <goldendevuz@gmail.com>
2026-03-07 16:15:34 +0500

    2026.03.07/16:15

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'zapchastop_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zapchastop_db',
+        'USER': 'carspare',      # matches POSTGRES_USER
+        'PASSWORD': 'secret',    # matches POSTGRES_PASSWORD
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
